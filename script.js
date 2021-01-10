834644839ee6b8650639c973369b8ba7c29308c1
LegoDynamite <oleg-dementei@tut.by>
2021-01-10 12:31:42 +0300

    Used while cycle

--- script.js
+++ script.js
@@ -10,19 +10,30 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
+/* for (let i = 0; i < 2; i++) {
   const a = prompt("Один из последних просмотренных фильмов?", ""),
         b = prompt("На сколько оцениваете его?", "");
-
   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
     personalMovieDB.movies[a] = b;
     console.log("done");
   } else {
       console.log('error'); 
       i--;
-
   }
+} */
 
+let i = 0;
+while (i < 0) {
+  i++;
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцениваете его?", "");
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+      console.log('error'); 
+      i--;
+  }
 }
 
 if (personalMovieDB.count < 10) {
